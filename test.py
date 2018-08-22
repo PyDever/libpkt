a66d89d0b463f010ef13c2c37cddaa6c39251714
Kaleb R. Horvath <bobafett2021@hotmail.com>
2018-08-22 13:09:09 -1000

    Update test.py

--- test.py
+++ test.py
@@ -5,10 +5,12 @@ import random
 # capture a random TCP packet from the network
 packet = psocket.s_sniff(max_bb=65565, cnt=3)[2]
 
-# disguise a new packet with new data as the old packet
+# disguise a new packet as the old packet
 new_packet = pkt.ip(
-    src_ip=pkt.rd_in(packet)['src_ip'], dst_ip=pkt.rd_in(packet)['dst_ip'],
-    ip_id=54321, d_hdr=random._urandom(1024)
+    src_ip=pkt.rd_in(packet)['src_ip'], 
+    dst_ip=pkt.rd_in(packet)['dst_ip'],
+    ip_id=54321, ip_ver=4, tcp_seq=454,
+    d_hdr=random._urandom(1024)
 )
 
 # send this new disguised packet off to the original packet's destination
