02e5c6f5c7a8a86c21db7421ab5af8b1db568875
Kaleb R. Horvath <bobafett2021@hotmail.com>
2018-08-22 13:04:20 -1000

    update libpkt.psocket

--- libpkt/psocket.py
+++ libpkt/psocket.py
@@ -1,22 +1,26 @@
 
 import socket # for network stuff
 import random # building packets
-from struct import *
+import struct # for packing headers
 
-import fcntl, array, time
+import fcntl  # file descriptor ops
+import array  # for building arrays
+import time   # for building timeouts
 
+# globals
 TCP_SOCK = socket.socket(socket.AF_INET,
     socket.SOCK_RAW, socket.IPPROTO_TCP)
 
-# this function will send a raw TCP/IP packet
 def send_to (packet, dst_ip='0.0.0.0', dst_p=0):
+    """this function will send raw TCP/IP packet"""
+
     try:
         TCP_SOCK.sendto(packet, (dst_ip, dst_p))
         return True 
     except socket.error: return False 
 
-# this function will sniff for TCP/IP packet traffic
 def s_sniff (max_bb=65565, cnt=2):
+    """ perform silent sniff"""
     try:
         packets = []
 
@@ -29,6 +33,7 @@ def s_sniff (max_bb=65565, cnt=2):
         return False
 
 def l_sniff (max_bb=65565, cnt=2):
+    """perform live sniff"""
     try:
         packets = []
 
